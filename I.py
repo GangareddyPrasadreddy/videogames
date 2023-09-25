3dd0cb4d73cd0b55f897f3db4a9464e7a173ffa0
GangareddyPrasadreddy <145973844+GangareddyPrasadreddy@users.noreply.github.com>
2023-09-25 18:27:12 +0530

    Add files via upload

---
+++ I.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(VIDEO)
+
+app.route("/")
+
+def home():
+    return render_template(r"index.html")
+
+
+if name=="main":
+    app.run(debug=False, port=5500)
